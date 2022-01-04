c3c50c3aac9c9c87850fa3831909368a878309c2
Mat Belmont <belmontmat@gmail.com>
2022-01-03 20:43:05 -0800

    added test skeletons

---
+++ ptcg-sim/__tests__/pack-opening.test.js
@@ -0,0 +1,13 @@
+import {breakPack, getRandomIntInclusive, fetchCards} from '../index.js';
+
+describe('breakPack', () => {
+  it('there are no dupes in a pack (minus the reverse)', () => {
+    //expect(2+2).toBe(4);
+  });
+  it('should always give 3 commons, 2 uncommons, a reverse(holo-), and a rare+ card', () => {
+
+  });
+  it('should be pulling from the correct set', () => {
+
+  });
+});
